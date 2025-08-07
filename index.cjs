e8601f15770f0f530be4d0ec019283717f56f6ff
allanbavajee <allan.bavajee@gmail.com>
2025-08-07 09:24:24 +0400

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -1,15 +1,9 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servir les fichiers statiques du dossier "public"
-app.use(express.static(path.join(__dirname, "public")));
-
-// Route racine pour envoyer la page d'accueil
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// Servir les fichiers statiques du dossier 'public'
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
